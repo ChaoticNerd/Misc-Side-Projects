f14f4d9112ac31d35d99ca0b431968a2781a074e
ChaoticNerd <nat.f.kho@gmail.com>
2025-10-14 23:59:39 -0700

    Update lab9
    I think I finished processingInput until fontLibrary is done.

--- CECS275/Lab9/main.cpp
+++ CECS275/Lab9/main.cpp
@@ -4,7 +4,17 @@
 int main(void){
 
     string userInput = getUserInput();
-    processUserInput(userInput);
+
+    // holding space for null char
+    char* userInputChars = new char[userInput.length() + 1];
+
+    processUserInput(userInput, userInputChars); // this function call breaks :: char* is a nullptr
+
+    /*///////////////////////////////// debug/testing //////////////////////////////////
+    cout << "*userInputChars produces: "    << *userInputChars      << endl; // this prints h (first char)
+    cout << "userInputChars produces "      << userInputChars       << endl; // this prints whatever is in userInput
+    cout << "userInputChars[3] produces "   << userInputChars[3]    << endl; // this prints whatever is in third index of userInput
+    //////////////////////////////////////////////////////////////////////////////////*/
 
     return 0;
 }
